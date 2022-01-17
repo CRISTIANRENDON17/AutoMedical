636be9fb70e8811d7feeb7abbd041e584fcfe712
darbey <dtejada12@misena.edu.co>
2022-01-17 09:15:44 -0500

    Agendamiento
    Se realiza la alerta de cita prioritaria y agendamiento.

--- src/components/Subcategories/Subcategories.jsx
+++ src/components/Subcategories/Subcategories.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import { Link, useParams } from 'react-router-dom';
 import CategoriesList from '../Categories/Categories.json';
@@ -10,7 +11,14 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faGoogleWallet} from '@fortawesome/free-brands-svg-icons';
 import {faHeart, faEye, faBone, faLungs, faBrain, faHeadSideCough, faAssistiveListeningSystems, faDiagnoses, 
         faHeadSideVirus, faAngry, faCarCrash, faBacteria, faShoePrints, faHeadSideMask, faSlash} from '@fortawesome/free-solid-svg-icons';
-
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { useNavigate } from 'react-router-dom';
 
 const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
@@ -19,16 +27,35 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
   }));
 
+  const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
+
 const Subcategories = () => {
+
+    const [open, setOpen] = useState(false);
+    const history = useNavigate();
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+      history('/Agendamiento')
+    };
+
     const {categoryName} = useParams();
     const [categories] = React.useState(CategoriesList);
     var categoriesIcons = [faHeart, faEye, faBone, faLungs, faBrain, faGoogleWallet, faHeadSideCough, faAssistiveListeningSystems, 
         faHeadSideMask, faShoePrints, faBacteria, faCarCrash, faDiagnoses, faHeadSideVirus, faAngry, faSlash];
+
+        
     return(
         <div>            
             <p>Selecciona en qué subcategoría se encuentra tu síntoma.</p>            
             <Link to="/SelfTriage"><ArrowBackIosIcon color="action" /></Link>
-            <Box sx={{ width: '100%', 'margin-top': '3%' }}>
+            <Box sx={{ width: '100%', 'marginTop': '3%' }}>
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                 {categories.map((category) => {
                         if(category.categoryName === categoryName){
@@ -38,7 +65,7 @@ const Subcategories = () => {
                                     category.subcategories.map((subcategory) => {
                                     return(                                
                                         <Grid item xs={12} md={6} lg={4} key={subcategory.idSubcategory}> 
-                                            <Item className='itemCategory'><FontAwesomeIcon icon ={categoriesIcons[category.id]} size='lg' className='iconCategory'/>{subcategory.subcategoryName}</Item>            
+                                            <Item className='itemCategory' onClick={handleClickOpen}><FontAwesomeIcon icon ={categoriesIcons[category.id]} size='lg' className='iconCategory'/>{subcategory.subcategoryName}</Item>                                            
                                         </Grid>
                                     );
                                 }));   
@@ -54,6 +81,28 @@ const Subcategories = () => {
                     })}
                 </Grid>
             </Box>
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle>{"Información"}</DialogTitle>
+                <DialogContent>
+                <DialogContentText id="alert-dialog-slide-description">
+                    De acuerdo con tus respuestas, la atención más adecuada para ti requiere una valoración prioritaria presencial,
+                    Por lo que te direccionamos a una  cita presencial prioritaria a tu IPS.
+                    <br></br>
+                    <br></br>
+                    <strong>¿Deseas realizar el agendamiento de tu cita?</strong>
+                </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={handleClose}>Cancelar</Button>
+                <Button onClick={handleClose}>Aceptar</Button>
+                </DialogActions>
+            </Dialog>
         </div>                
     );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
+import {MuiPickersUtilsProvider} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import esLocale from 'date-fns/locale/es';
 
 
 	ReactDOM.render(
 		<React.StrictMode>
-			<App/>
+			<MuiPickersUtilsProvider utils={DateFnsUtils} locale={esLocale}>
+				<App/>
+			</MuiPickersUtilsProvider>			
 		</React.StrictMode>,
 	document.getElementById('root'));
 
