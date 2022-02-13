c50f9d8fdb4741d43a71481d7755e90f2ff0c125
Cristian <cristianrendon1704@gmail.com>
2022-02-13 16:34:17 -0500

    Revert "Arreglos de algunos warnings y quitar columnas addres y telefono"
    This reverts commit 452eace11d5a2231f3ab0b6e7180bed55ee58507.

--- src/components/ListUser/ListUser.js
+++ src/components/ListUser/ListUser.js
@@ -12,10 +12,10 @@ import { getCollection, getIdUser, updateFieldUser } from "../../actions";
 import { useEffect } from "react";
 import { useState } from "react";
 import { Box } from "@mui/material";
-//import { addSeconds } from "date-fns/esm";
+import { addSeconds } from "date-fns/esm";
 import Button from '@mui/material/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faDatabase, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 
 function Herramientas() {
@@ -209,6 +209,19 @@ const columnsDoctor = [
     editable: false,
   },
   {
+    field: "phoneNumber",
+    headerName: "Teléfono",
+    width: 100,
+    editable: false,
+  },
+  {
+    field: "address",
+    headerName: "Dirección",
+    dataGeneratorUniquenessEnabled: true,
+    width: 200,
+    editable: false,
+  },
+  {
     field: "rol",
     headerName: "Rol",
     dataGeneratorUniquenessEnabled: true,
@@ -260,6 +273,19 @@ const columnsAdmin = [
     editable: true,
   },
   {
+    field: "phoneNumber",
+    headerName: "Teléfono",
+    width: 100,
+    editable: true,
+  },
+  {
+    field: "address",
+    headerName: "Dirección",
+    dataGeneratorUniquenessEnabled: true,
+    width: 200,
+    editable: true,
+  },
+  {
     field: "rol",
     headerName: "Rol",
     dataGeneratorUniquenessEnabled: true,
@@ -295,7 +321,7 @@ const columnsAdmin = [
               value:thisRow.estado,
             };
             //console.log("Data a actualizar: ", data);
-            await updateFieldUser(data);
+            const statusUpdate = await updateFieldUser(data);
             //console.log("Estado actualización: ", statusUpdate);
           //console.log(JSON.stringify(thisRow, null, 4))
         //return alert(JSON.stringify(thisRow, null, 4));
@@ -305,7 +331,7 @@ const columnsAdmin = [
       //console.log(params.row.estado);
       var estado = params.row.estado; 
       //console.log(estado);
-      return <Button onClick={onClick}>{estado !== 'Activo' ? 'Inactivo' : 'Activo'}</Button>;
+      return <Button onClick={onClick}>{estado != 'Activo' ? 'Inactivo' : 'Activo'}</Button>;
     },
     /*renderCell: () =>{
       const [estado, setEstado] = useState('Inactivo');
--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,13 @@
 import {initializeApp} from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCorK1GHH7vsnE72l0KjC4fxf-wz9e7u6I",
-//   authDomain: "automedical-66123.firebaseapp.com",
-//   projectId: "automedical-66123",
-//   storageBucket: "automedical-66123.appspot.com",
-//   messagingSenderId: "495137405001",
-//   appId: "1:495137405001:web:2f1631267bcb8724cbe798"
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDQsctwnf0aAcfyh9A6KU6B63iNCP2tFYk",
-  authDomain: "automedical-e320a.firebaseapp.com",
-  projectId: "automedical-e320a",
-  storageBucket: "automedical-e320a.appspot.com",
-  messagingSenderId: "513376818576",
-  appId: "1:513376818576:web:a609d27f7267da32d00f15"
+  apiKey: "AIzaSyCorK1GHH7vsnE72l0KjC4fxf-wz9e7u6I",
+  authDomain: "automedical-66123.firebaseapp.com",
+  projectId: "automedical-66123",
+  storageBucket: "automedical-66123.appspot.com",
+  messagingSenderId: "495137405001",
+  appId: "1:495137405001:web:2f1631267bcb8724cbe798"
 };
 
 const app = initializeApp(firebaseConfig);
